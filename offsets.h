1669339aeb664b3461cabd9eb5759c7d2ed53e14
Proofex-1 <141692791+Proofex-1@users.noreply.github.com>
2023-10-06 10:11:40 -0700

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,1 +1,25 @@
-
+UWorldState = 0xA16A100;
+UWorldKey = 0xA16A138;
+LineOfSight = 0x58A1620;
+TriggerVEH = 0x1E14750;
+ProcessEvent = 0x3DC8E40;
+GetSpreadAngles = 0x34097A0;
+ToVectorAndNormalize = 0x3AD9300;
+ToAngleAndNormalize = 0x3AD5AF0;
+FNamePool = 0x9F32140;
+FNamepoolDecryptWide = 0x3B59390;
+FNamepoolDecryptNonWide = 0x3B59320;
+GObjectState = 0x9F8CFB8;
+GObjectKey = 0x9F8CFF0;
+StaticFindObject = 0x3DDD4E0;
+StaticLoadObject = 0x3DDDF90;
+BoneMatrix = 0x5873310;
+AresOutline = 0x58222D0;
+GetActorBounds = 0x5645450;
+ProjectWorldToScreen = 0x59C1930;
+ 
+ 
+CharacterMinimapComponent = 0x1190
+PortraitMinimapComponent = 0x1198
+DamageHandler = 0xA00;
+Inventory = 0x9A0;
